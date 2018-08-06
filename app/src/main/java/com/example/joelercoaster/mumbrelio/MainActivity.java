c6b0a8e658fadb054afd299a5129aec931ee5fb3
Joel Williams <joel.b.williams.is@gmail.com>
2018-08-06 08:47:10 -0500

    forecast return on view

--- app/src/main/java/com/example/joelercoaster/mumbrelio/MainActivity.java
+++ app/src/main/java/com/example/joelercoaster/mumbrelio/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.joelercoaster.mumbrelio;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -17,13 +19,66 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvTemp;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tvTemp = findViewById(R.id.tvTemp);
+        tvTemp = findViewById(R.id.tvTemp);
 
-        new ForecastTask().execute();
+        ForecastTask forecastTask = new ForecastTask();
+        forecastTask.execute();
+    }
+
+    class ForecastTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            //api.darksky.net/forecast/[key]/[latitude],[longitude]
+            String response;
+            try {
+                URL url = new URL("https://api.darksky.net/forecast/cc49555736076755c5dee363e2b1fd21/41.8781,87.6298");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                try {
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    response = readStream(in);
+                    Log.d("LOOK AT ME", response);
+                    return response;
+                } catch(IOException e){
+
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (MalformedURLException e) {
+
+            } catch (IOException e) {
+
+            }
+            return "Forecast Task Finished Without Response";
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            tvTemp.setText(response);
+        }
+
+        private String readStream(InputStream is) {
+            try {
+                ByteArrayOutputStream bo = new ByteArrayOutputStream();
+                int i = is.read();
+                while (i != -1) {
+                    bo.write(i);
+                    i = is.read();
+                }
+                return bo.toString();
+            } catch (IOException e) {
+                return "";
+            }
+        }
     }
 }
+
+
