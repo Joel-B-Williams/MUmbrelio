8d21abd2e6f766578e84ee86261b9821e9eb78da
Joel Williams <joel.b.williams.is@gmail.com>
2018-09-30 14:20:55 -0500

    check for fine location permissions

--- app/src/main/java/com/example/joelercoaster/mumbrelio/MainActivity.java
+++ app/src/main/java/com/example/joelercoaster/mumbrelio/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.joelercoaster.mumbrelio;
 
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -28,6 +30,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    String noPermissions = "You have not granted location permissions";
+                    tvTemp.setText(noPermissions);
+                    return;
+                }
                 client.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
